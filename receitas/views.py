1812b64b6b246d20a32b58d43ef0b8c785c44fce
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2023-04-26 19:09:07 -0300

    Projeto Receitas - Templates

--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.receitas.fabrica import make_recipe
 
 #HTTP Request
 def home(request):
     return render(request, 'receitas/pages/home.html', context={
-        'nome':'Fábio Rodrigues de Moraes',
+        'receitas':[make_recipe() for _ in range(10)],
     })
     #HTTP Response
 
-
+def receita(request, id):
+    return render(request, 'receitas/pages/receita-view.html', context={
+        'receita':make_recipe(),
+        'is_detail_page':True,
+    })
