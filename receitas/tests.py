122069474b8625e7ce67af34a02c599cbb3f30ec
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2023-05-16 20:29:33 -0300

    Usando pytest

--- receitas/tests.py
+++ receitas/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ReceitaURLSTest(TestCase):
+    def test_receitas_home_url_esta_correto(self):
+        url = reverse('receitas-home')
+        self.assertEqual(url, '/')
+
+    def test_receitas_categoria_url_esta_correto(self):
+        url = reverse('categoria', kwargs={'categoria_id': 1})
+        self.assertEqual(url, '/receitas/categoria/1/')
+
+    def test_receitas_receita_url_esta_correto(self):
+        url = reverse('receitas-receita', kwargs={'id': 1})
+        self.assertEqual(url, '/receitas/1/')
