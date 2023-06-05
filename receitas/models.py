e9e7ac9c62be3e1f16af10873ab0b76a9f43ee40
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2023-06-05 19:44:27 -0300

    Ajustando o slug para unique

--- receitas/models.py
+++ receitas/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=65)
 
     def __str__(self):
         return self.nome
 
+
 class Receitas(models.Model):
     titulo = models.CharField(max_length=65)
     descricao = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     tempo_preparo = models.IntegerField()
     unidade_tempo_preparo = models.CharField(max_length=65)
     porcoes = models.IntegerField()
@@ -20,13 +22,13 @@ class Receitas(models.Model):
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
     esta_publicado = models.BooleanField(default=False)
-    receita_imagem = models.ImageField(upload_to='receitas/receitas_imagens/%d/%m/%Y/', 
+    receita_imagem = models.ImageField(upload_to='receitas/receitas_imagens/%d/%m/%Y/',
                                        blank=True, default=''
                                        )
     categoria = models.ForeignKey(
         Categoria, on_delete=models.SET_NULL, null=True, blank=True,
         default=None
-        )
+    )
     autor = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
